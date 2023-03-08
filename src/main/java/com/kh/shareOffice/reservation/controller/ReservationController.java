e2cda8ceb63cf060998001a49b1fe755c560fee9
Sangeun <p9s9e9@naver.com>
2023-03-08 23:52:40 +0900

    ADD : Payment 네임 변경, 강남 1호점 JSP 상세페이지 완성.
    1.
    Payment 관련 폴더, 매퍼 등 reservation으로 리네임.
    
    2.
    강남 1호점 프론트 작업 완료.

---
+++ src/main/java/com/kh/shareOffice/reservation/controller/ReservationController.java
@@ -0,0 +1,16 @@
+package com.kh.shareOffice.reservation.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ReservationController {
+
+	@RequestMapping(value = "/gangnam1", method = RequestMethod.GET)
+	public String gangnam1() {
+		
+		return "reservation/office/gangnam1";
+	}
+}
